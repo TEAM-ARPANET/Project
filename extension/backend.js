1ab94d275debe5ba89581035ccc573e44feec2a9
HuntaBadday <HuntaBadday@gmail.com>
2026-03-29 00:18:29 -0300

    Makes code runnable and fixes indentation

--- extension/backend.js
+++ extension/backend.js
@@ -3,11 +3,10 @@
  * "manifest.json must be updated to test your server code" <- CHECK THIS
  */
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if(msg?.type !== "ANALYZE_IMAGE_URL") return;
-
-    (async () => {
-        try {
+chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
+    if(msg?.type !== "ANALYZE") return;
+    try {
+        
         const imgRes = await fetch(msg.url);
         if(!imgRes.ok) throw new Error(`Image fetch failed: ${imgRes.status}`);
         
@@ -15,26 +14,24 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         const form = new formData();
         
         form.append("image", blobRep, "image.jpg");
-        const serverRes = await fetch("http://mapd.cs-smu,ca:6502/analyze", {
+        const serverRes = await fetch("http://localhost:6502/analyze", {
             method: "POST",
-            body: form,
+            body: form
         });
-
+        
         if(!serverRes.ok) {
             const txt = await serverRes.text().catch(() => "");
             throw new Error(`Server failed: ${serverRes.status} ${txt}`);
         }
-
+        
         const data = await serverRes.json();
         sendResponse({
             ok: true,
-            description: data.description || "(no description)",
+            description: data.contents || "(no description)"
         });
-
-        } catch (err) {
+    } catch (err) {
         console.error(err);
         sendResponse({ok: false, error: String(err?.message || err) });
-        }
-    })();
+    }
     return true;
 });
